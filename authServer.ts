98ddcb55948e081d4bea485313f628c36a1cabfa
duransbo <duransbo@gmail.com>
2023-04-13 16:32:38 -0300

    deploy 4

--- authServer.ts
+++ authServer.ts
@@ -1,48 +1,47 @@
-import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
-import { load } from "https://deno.land/std/dotenv/mod.ts";
-import { renderFileToString } from "https://deno.land/x/dejs@0.10.3/mod.ts";
-import { getAccessToken, getProfileInfo } from "./googleLogin.ts"
+import { load } from "https://deno.land/std@0.183.0/dotenv/mod.ts";
+import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import { getAccessToken, getProfileInfo } from "./googleLogin.ts";
 
 const env = await load();
-const clientId = env["CLIENT_ID"];
-const clientSecret = env["CLIENT_SECRET"];
-const redirectUrl = env["REDIRECT_URL"];
-
-let google_oauth_url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+const google_oauth_url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
 google_oauth_url.searchParams.set("scope", "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email");
-google_oauth_url.searchParams.set("redirect_uri", redirectUrl);
+google_oauth_url.searchParams.set("redirect_uri", env["REDIRECT_URL"]);
 google_oauth_url.searchParams.set("response_type", "code");
-google_oauth_url.searchParams.set("client_id", clientId);
+google_oauth_url.searchParams.set("client_id", env["CLIENT_ID"]);
 google_oauth_url.searchParams.set("access_type", "online");
 
-console.info("auth server listening on port 8000");
-const server = serve({ port: 8000 });
+async function render(filePath: string, contextType: string) {
+	const file = await Deno.readFile(filePath);
 
-for await (const request of server) {
-    let request_url = new URL(request.url, "https://duran-auth.deno.dev:8000");
-   
-    switch(request_url.pathname) {
-    	case "/":
-    		// send google oauth url to the template
-    		let html = await renderFileToString("views/index.html", { google_oauth_url: google_oauth_url.toString() });
-    		request.respond({ status: 200, body: html });
-    		break;
-
-    	case "/google-auth":
-    		try {
-    			let access_token = await getAccessToken(clientId, clientSecret, redirectUrl, request_url.searchParams.get("code"));
-    			let profile_info = await getProfileInfo(access_token);
-    			
-    			// send profile info to the template
-    			let html = await renderFileToString("views/google-auth.html", { profile_info: profile_info });
-	    		request.respond({ status: 200, body: html });
-    		}
-			catch(error) {
-				// send error message to the template
-				let html = await renderFileToString("views/google-auth.html", { error: error.message });
-	    		request.respond({ status: 200, body: html });
-			}
-
-    		break;
-    }
+	return new Response(file, {
+		headers: { "content-type": contextType }
+	});
 }
+
+async function router(request: Request): Promise<Response> {
+	const url = new URL(request.url, "http://duran-auth.deno.dev:8000");
+
+	switch(url.pathname) {
+		case "/":
+			return Response.redirect(google_oauth_url.toString(), 307);
+		case "/google-auth":
+			const access_token = await getAccessToken(env["CLIENT_ID"], env["CLIENT_SECRET"], env["REDIRECT_URL"], url.searchParams.get("code"));
+			const profile_info = await getProfileInfo(access_token);
+
+			const json = {
+				id : profile_info['id'],
+				name : profile_info['name'],
+				mail : profile_info['mail']
+			};
+
+			return new Response(`<script>window.opener.postMessage(${JSON.stringify(json)}, "http://192.168.15.5:7000");</script>`, {
+				headers: { "content-type": "text/html" }
+			});
+		default:
+			return render("index.html", "text/html");
+	}
+
+	
+}
+
+serve(router, { port : 8000 });
